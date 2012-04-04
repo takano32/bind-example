3df4ace49d7f61f873d65de27754a3fb3bb1df95
TAKANO Mitsuhiro <takano32@gmail.com>
2012-04-04 09:37:06 +0900

    匿名関数の callee で再帰呼び出ししたい

--- bind-example.js
+++ bind-example.js
@@ -5,8 +5,24 @@ func = function(n) {
 }
 
 
+errFunc = function(err, n, retry) {
+	err = true; /* なぜかかならず失敗する設定 */
+	if (retry == 0) return;
+	if (err == true) {
+		errFunc(err, n, retry - 1);
+	}
+	console.log("Retry: " + n);
+}
+
 for (i = 0; i < 10; i++) {
-	f = func.bind(this, i)
+	// f = func.bind(this, i)
+	// setTimeout(f, i * 100);
+	n = i;
+	retry = 3;
+	f = errFunc.bind(this, false, n, retry);
 	setTimeout(f, i * 100);
 }
+
+/* こういうのをかっこよく Node.js で書くにはどうしてるんですか？ */
+/* # かっこいいの定義：匿名関数の callee で再帰呼び出ししたい */
 
